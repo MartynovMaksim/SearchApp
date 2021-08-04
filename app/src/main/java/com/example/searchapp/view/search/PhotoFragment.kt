ebf627390adc48d94c3be61c4215dfce686a82e1
user <user>
2021-08-04 21:24:12 +0300

    add "correct" image dragging

--- app/src/main/java/com/example/searchapp/view/search/PhotoFragment.kt
+++ app/src/main/java/com/example/searchapp/view/search/PhotoFragment.kt
@@ -1,10 +1,9 @@
 package com.example.searchapp.view.search
 
-import android.content.ClipData
-import android.content.ClipDescription
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import com.bumptech.glide.Glide
@@ -17,6 +16,8 @@ class PhotoFragment : Fragment() {
     private var _binding: FragmentPhotoBinding? = null
     private val binding get() = requireNotNull(_binding)
     private var photoUrl: String? = null
+    private var lastTouchX: Float = 0F
+    private var lastTouchY: Float = 0F
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,22 +30,32 @@ class PhotoFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentPhotoBinding.inflate(inflater,container, false)
+        _binding = FragmentPhotoBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         with(binding) {
-            Glide.with(requireContext()).load(photoUrl).placeholder(R.drawable.ic_baseline_image_24).into(photo)
-            photo.setOnLongClickListener {
-                val clipText = "Move Image"
-                val item = ClipData.Item(clipText)
-                val mimeTypes = arrayOf(ClipDescription.MIMETYPE_TEXT_PLAIN)
-                val data = ClipData(clipText,mimeTypes, item)
+            Glide.with(requireContext()).load(photoUrl).placeholder(R.drawable.ic_baseline_image_24)
+                .into(photo)
 
-                val dragShadowBuilder = View.DragShadowBuilder(it)
-                it.startDragAndDrop(data,dragShadowBuilder,it,0)
+            photo.setOnTouchListener { v, event ->
+                when (event.action) {
+                    MotionEvent.ACTION_DOWN -> {
+                        lastTouchX = event.x
+                        lastTouchY = event.y
+                    }
+                    MotionEvent.ACTION_MOVE -> {
+                        var posX = event.x - lastTouchX
+                        var posY = event.y - lastTouchY
+                        v.x = (v.x + posX)
+                        v.y = (v.y + posY)
+//                        v.invalidate()
+                    }
+                }
+                lastTouchX = event.x
+                lastTouchY = event.y
                 true
             }
         }
