1a0c15141361e3588339f6debf5523950a5d8dc9
user <user>
2021-08-05 11:48:51 +0300

    change getting touch coordinates to rawX and rawY

--- app/src/main/java/com/example/searchapp/view/search/PhotoFragment.kt
+++ app/src/main/java/com/example/searchapp/view/search/PhotoFragment.kt
@@ -1,5 +1,6 @@
 package com.example.searchapp.view.search
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,7 +8,6 @@ import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import com.bumptech.glide.Glide
-import com.example.searchapp.R
 import com.example.searchapp.databinding.FragmentPhotoBinding
 
 private const val PHOTO_URL = "param1"
@@ -34,28 +34,27 @@ class PhotoFragment : Fragment() {
         return binding.root
     }
 
+    @SuppressLint("ClickableViewAccessibility") // don't support performClick for photo
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         with(binding) {
-            Glide.with(requireContext()).load(photoUrl).placeholder(R.drawable.ic_baseline_image_24)
-                .into(photo)
+            Glide.with(requireContext()).load(photoUrl).into(photo)
 
             photo.setOnTouchListener { v, event ->
                 when (event.action) {
                     MotionEvent.ACTION_DOWN -> {
-                        lastTouchX = event.x
-                        lastTouchY = event.y
+                        lastTouchX = event.rawX
+                        lastTouchY = event.rawY
                     }
                     MotionEvent.ACTION_MOVE -> {
-                        var posX = event.x - lastTouchX
-                        var posY = event.y - lastTouchY
-                        v.x = (v.x + posX)
-                        v.y = (v.y + posY)
-//                        v.invalidate()
+                        val posX = event.rawX - lastTouchX
+                        val posY = event.rawY - lastTouchY
+                        v.x += posX
+                        v.y += posY
                     }
                 }
-                lastTouchX = event.x
-                lastTouchY = event.y
+                lastTouchX = event.rawX
+                lastTouchY = event.rawY
                 true
             }
         }
