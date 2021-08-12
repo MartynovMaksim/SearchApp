0e60bb92cd92ae395ebf96fef9ff429456bc7944
Martynov Maksim <martynmaks@gmail.com>
2021-08-12 12:23:04 +0300

    make photo moving logic more clear and make animation for alpha and photo

--- app/src/main/java/com/example/searchapp/view/search/PhotoFragment.kt
+++ app/src/main/java/com/example/searchapp/view/search/PhotoFragment.kt
@@ -1,5 +1,7 @@
 package com.example.searchapp.view.search
 
+import android.animation.AnimatorSet
+import android.animation.ValueAnimator
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -9,7 +11,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import com.example.searchapp.databinding.FragmentPhotoBinding
-import kotlin.math.round
+import kotlin.math.abs
 
 private const val PHOTO_URL = "param1"
 
@@ -17,7 +19,7 @@ class PhotoFragment : Fragment() {
     private var _binding: FragmentPhotoBinding? = null
     private val binding get() = requireNotNull(_binding)
     private var photoUrl: String? = null
-    private var lastTouchY: Float = 0F
+    private val TAG = "PhotoFragment"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,41 +41,44 @@ class PhotoFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         with(binding) {
             Glide.with(requireContext()).load(photoUrl).into(photo)
-
+            var lastTouchY = 0F
+            var alpha = 0F
+            view.background.alpha = 255
+            val animatorSet = AnimatorSet()
             photo.setOnTouchListener { v, event ->
                 when (event.action) {
                     MotionEvent.ACTION_DOWN -> {
-                        lastTouchY = event.rawY
+                        if (!animatorSet.isRunning) {
+                            lastTouchY = event.rawY
+                        }
                     }
                     MotionEvent.ACTION_MOVE -> {
                         val posY = event.rawY - lastTouchY
-                        val dH = (v.top - view.top) * 2.0F
-                        if (v.y + posY < dH / 2) {
-                            val floatAlpha = ((v.y + posY).coerceIn(0F, dH / 2) / (dH / 2))
-                            view.background.alpha = round(floatAlpha * 255).toInt()
-                        } else {
-                            val floatAlpha = ((dH - ((v.y + posY).coerceIn(dH / 2, dH))) / (dH / 2))
-                            view.background.alpha = round(floatAlpha * 255).toInt()
-                        }
-                        if (v.y + posY < 0) {
-                            activity?.supportFragmentManager?.popBackStack()
-                        } else if (v.y + posY > dH) {
-                            activity?.supportFragmentManager?.popBackStack()
-                        }
-                        v.y = (v.y + posY)
+                        v.y += posY
+                        lastTouchY = event.rawY
+                        alpha = (1 - (abs(v.top - v.y)) / v.top).coerceIn(0F, 1F)
+                        view.background.alpha = (255 * alpha).toInt()
                     }
                     MotionEvent.ACTION_UP -> {
-                        view.animate().apply {
-                            duration = 1000
-                            view.background.alpha = 255
-                        }
-                        v.animate().apply {
-                            duration = 1000
-                            y(v.top.toFloat())
+                        if (alpha == 0F || alpha == 1F) {
+                            activity?.supportFragmentManager?.popBackStack()
+                        } else {
+                            val animatedAlpha = ValueAnimator.ofFloat(alpha, 1F)
+                            animatedAlpha.addUpdateListener {
+                                view.background.alpha = (255 * (it.animatedValue as Float)).toInt()
+                            }
+                            val animatedPhoto = ValueAnimator.ofFloat(v.y, v.top.toFloat())
+                            animatedPhoto.addUpdateListener {
+                                v.y = it.animatedValue as Float
+                            }
+                            animatorSet.apply {
+                                duration = 1000
+                                playTogether(animatedAlpha, animatedPhoto)
+                                start()
+                            }
                         }
                     }
                 }
-                lastTouchY = event.rawY
                 true
             }
         }
