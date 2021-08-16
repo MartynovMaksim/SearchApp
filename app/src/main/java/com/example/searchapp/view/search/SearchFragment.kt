a9baa4d2054a8056c7face0482866b97b7f5e670
Martynov Maksim <martynmaks@gmail.com>
2021-08-16 10:52:32 +0300

    remove searchButton

--- app/src/main/java/com/example/searchapp/view/search/SearchFragment.kt
+++ app/src/main/java/com/example/searchapp/view/search/SearchFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.doAfterTextChanged
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
@@ -48,8 +49,10 @@ class SearchFragment : Fragment(), ItemClickListener {
             recyclerView.layoutManager = GridLayoutManager(activity, 2)
             SearchAdapter(requireContext(), this@SearchFragment).also { adapter = it }
             recyclerView.adapter = adapter
-            searchButton.setOnClickListener {
-                viewModel.search(searchEditText.text.toString())
+            searchEditText.doAfterTextChanged {
+                if (requireNotNull(it?.length) > 2) {
+                    viewModel.search(searchEditText.text.toString())
+                }
             }
 
         }
